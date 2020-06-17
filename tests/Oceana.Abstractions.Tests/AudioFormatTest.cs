5edb667f07051841396491568a17350461cd8aab
Maurice Davidson <mo09890@googlemail.com>
2020-06-17 20:25:19 +0100

    Add tests for AudioFormat

---
+++ tests/Oceana.Abstractions.Tests/AudioFormatTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Shouldly;
+using Xunit;
+
+namespace Oceana.Abstractions.Tests
+{
+    public class AudioFormatTest
+    {
+        [Fact]
+        public void EqualsOperatorShouldReturnCorrectly()
+        {
+            var left = new AudioFormat(1, 10);
+            var right = new AudioFormat(1, 10);
+
+            (left == right).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void NotEqualOperatorShouldReturnCorrectlyForDifferingChannelWidth()
+        {
+            var left = new AudioFormat(1, 10);
+            var right = new AudioFormat(2, 10);
+
+            (left != right).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void NotEqualOperatorShouldReturnCorrectlyForDifferingSampleRate()
+        {
+            var left = new AudioFormat(1, 10);
+            var right = new AudioFormat(1, 20);
+
+            (left != right).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void GetHashCodeShouldReturnCorrectly()
+        {
+            new AudioFormat(1, 10).GetHashCode().ShouldBe(1 ^ 10);
+        }
+    }
+}
